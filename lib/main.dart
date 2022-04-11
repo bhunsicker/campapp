d68bffbdf15585c28bf9a04a17af8c5f3834317c
bhunsicker <whunsickeriii@satx.rr.com>
2022-04-10 21:30:05 -0500

    Initial Stub Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-//import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CampApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class CampApp extends StatelessWidget {
+  CampApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,78 +13,147 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Camp App Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Home Page'),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class CalendarPage extends StatelessWidget {
+  CalendarPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Calendar Page'),
+      ),
+    );
   }
+}
 
-  Widget appDrawer(BuildContext context) {
+class PhotoPage extends StatelessWidget {
+  PhotoPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Photo Page'),
+      ),
+    );
+  }
+}
+
+class ChatPage extends StatelessWidget {
+  ChatPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Chat Page'),
+      ),
+    );
+  }
+}
+
+class LocatorPage extends StatelessWidget {
+  LocatorPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Locator Page'),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  SettingsPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text('Camp App Settings Page'),
+      ),
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  AppDrawer({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(padding: EdgeInsets.zero, children: const [
-        UserAccountsDrawerHeader(
-          accountEmail: Text('whusickeriii@satx.rr.com'),
-          accountName: Text('Bill Hunsicker'),
-          currentAccountPicture: Text('Picture Here'),
-        ),
-        ListTile(leading: Icon(Icons.calendar_month), title: Text('Calendar'), onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => PageTwo(),)) ,
-        ListTile(
-            leading: Icon(Icons.photo_album), title: Text('Upload Pictures')),
-        ListTile(leading: Icon(Icons.chat_bubble), title: Text('Chat')),
-        ListTile(leading: Icon(Icons.location_pin), title: Text('Locator')),
-      ]),
-    );
-  }
-
-class PageTwo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Burger Time!'),
+        child: Column(children: <Widget>[
+      Expanded(
+        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountEmail: Text('whusickeriii@satx.rr.com'),
+            accountName: Text('Bill Hunsicker'),
+            currentAccountPicture: Text('Picture Here'),
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Calendar'),
+            onTap: () => Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new CalendarPage())),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Upload Pictures'),
+            onTap: () => Navigator.push(context,
+                new MaterialPageRoute(builder: (context) => new PhotoPage())),
+          ),
+          ListTile(
+            leading: Icon(Icons.chat_bubble),
+            title: Text('Chat'),
+            onTap: () => Navigator.push(context,
+                new MaterialPageRoute(builder: (context) => new ChatPage())),
+          ),
+          ListTile(
+            leading: Icon(Icons.location_pin),
+            title: Text('Locator'),
+            onTap: () => Navigator.push(context,
+                new MaterialPageRoute(builder: (context) => new LocatorPage())),
+          )
+        ]),
       ),
-      body: Center(
-        child: Text('Page TWO /flex'),
-      ),
-      drawer: MyDrawerDirectory(), // ← Drawer Directory a.k.a. burger icon
-    );
-  }
-}
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      drawer: appDrawer(context),
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-      ),
-    );
+      Container(
+          child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                  child: Column(
+                children: <Widget>[
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                    onTap: () => Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new SettingsPage())),
+                  ),
+                ],
+              ))))
+    ]));
   }
 }
